96f3e7bc578990f84a2b5f15ae4c41d60e0c0881
Claire Bohlen <clairebohlen@gmail.com>
2021-01-14 12:37:38 -0700

    todo filter

--- app.js
+++ app.js
@@ -68,12 +68,21 @@ function filterTodo(e){
         switch(e.target.value){
             case "all":
                 todo.style.display = "flex";
+            break;
             case "completed":
             if (todo.classList.contains('completed')){
                 todo.style.display = "flex";
             } else{
                 todo.style.display = "none";
             }
+            break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+            break;
         }
     })
 }
