aa283bc613e0a6c99326e538ba3f60c5aff75a2c
Claire Bohlen <clairebohlen@gmail.com>
2021-01-24 12:26:23 -0700

    adding sudo code

---
+++ practice/app.js
@@ -0,0 +1,20 @@
+//Dom Variables
+
+//Todo Input
+//Input Icon
+//Div Container & UL
+//Filter Dropdown
+
+
+
+//Event Listeners
+
+//Todo Input Button
+//Todo List Delete & Completed Buttons
+//Dropdown Selector
+
+
+//Functions
+
+
+
