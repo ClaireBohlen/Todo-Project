a663fd4d052fc7a7d71e9f69e4649cb05f892358
Claire Bohlen <clairebohlen@gmail.com>
2021-01-26 15:48:31 -0700

    adding a completed button in JS

--- practice/app.js
+++ practice/app.js
@@ -32,10 +32,25 @@ function addTodo(event){
     //Dynamicallly create the todo list
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
+
     //Create the list inside the todoDiv
     const newTodo = document.createElement('li');
     //The text for the li is = to the user input 
     newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    //make the todo a child el of todoDiv
+    todoDiv.appendChild(newTodo);
+
+    //ADD LOCAL STORAGE HERE LATER
+
+    //Dynamically create completed button
+    const completedButton = document.createElement('button');
+    completedButton.innerText = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    //make this button a child el of todoDiv
+    todoDiv.appendChild(completedButton);
+
+
 
 
 
